febf2d67454eb9764655ee49fcd375076da4f4d5
Sharon Cai <70926723+sharon-cai@users.noreply.github.com>
2021-06-27 01:32:25 -0400

    Add files via upload
    js POST blob to django

--- Web Development/scripts/video.js
+++ Web Development/scripts/video.js
@@ -3,10 +3,12 @@ let video = document.querySelector("#video");
 let start_button = document.querySelector("#start-record");
 let stop_button = document.querySelector("#stop-record");
 let download_link = document.querySelector("#download-video");
+let submit = document.querySelector("#submit");
 
 let camera_stream = null;
 let media_recorder = null;
 let blobs_recorded = [];
+let video_local = null;
 
 camera_start_button.addEventListener('click', async function() {
    	camera_stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });    // security option to grant or deny access to their cameras or microphones
@@ -14,6 +16,7 @@ camera_start_button.addEventListener('click', async function() {
 });
 
 start_button.addEventListener('click', function() {
+    console.log("here")
     // set MIME type of recording as video/webm
     media_recorder = new MediaRecorder(camera_stream, { mimeType: 'video/webm' });
 
@@ -25,8 +28,9 @@ start_button.addEventListener('click', function() {
     // event : recording stopped & all blobs sent
     media_recorder.addEventListener('stop', function() {
     	// create local object URL from the recorded video blobs
-    	let video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
+    	video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
     	download_link.href = video_local;
+        console.log(blobs_recorded)
     });
 
     // start recording with each recorded blob having 1 second video
@@ -40,4 +44,26 @@ function stop(stream) {
 stop_button.addEventListener('click', function() {
 	media_recorder.stop(); 
     stop(video.srcObject);
+});
+
+submit.addEventListener('click', function() {
+    var blob = new Blob(blobs_recorded, { 'type' : 'video/webm' });
+
+    console.log("start sending binary data...");
+    var form = new FormData();
+    form.append('video', blob);
+
+    $.ajax({
+        url: 'http://localhost:8000/api/upload/file/',
+        type: 'POST',
+        data: form,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            console.log('response' + JSON.stringify(data));
+        },
+        error: function () {
+        // handle error case here
+        }
+    });
 });
